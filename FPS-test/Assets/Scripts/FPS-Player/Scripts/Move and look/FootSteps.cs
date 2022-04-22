0a838ae326aa2ecfcaf7199f3bce55df5712e336
jozsef joba. bali <joba@pc-tec.local>
2022-04-22 14:11:54 +0200

    footsteps on stone

--- FPS-test/Assets/Scripts/FPS-Player/Scripts/Move and look/FootSteps.cs
+++ FPS-test/Assets/Scripts/FPS-Player/Scripts/Move and look/FootSteps.cs
@@ -6,33 +6,50 @@ public class FootSteps : MonoBehaviour
 {
     // This script have to be attached on tha player object.
 
-
-    Movement movement;
+    Movement movement;  // MOVEMENT SCRIPT
     private AudioSource audioSource;
-
+    private AudioSource stoneSteps;
+    public Transform cube;
+    private string floorTag;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         movement = GetComponent<Movement>();
         audioSource = GetComponent<AudioSource>();
+        stoneSteps = cube.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit))
+        {
+            //This is the colliders tag
+            floorTag = hit.collider.tag;
+            Debug.Log(floorTag);
+        }
+
         if (movement.isWalking == true)
         {
             audioSource.pitch = Random.Range(0.6f, 1.2f);
-            if (!audioSource.isPlaying)
+            if (!audioSource.isPlaying && floorTag == "ForestFloor")
             {
                 audioSource.Play();
+                stoneSteps.Stop();
             }
-         
+            if (!stoneSteps.isPlaying && floorTag == "Stone")
+            {
+                audioSource.Stop();
+                stoneSteps.Play();
+            }
         }
         else
         {
             audioSource.Stop();
+            stoneSteps.Stop();
         }
+
     }
 }
